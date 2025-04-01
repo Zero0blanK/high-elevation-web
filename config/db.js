b2cf879f2a76bc7d7568959bf43ea6aa9c62f306
Mystyvyy <75249731+Zero0blanK@users.noreply.github.com>
2025-04-01 20:10:26 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'shuttle.proxy.rlwy.net',
+  user: 'root',
+  password: '',
+  database: 'railway',
+  port: '5432',
   waitForConnections: true,
   connectionLimit: 10, // Maximum number of connections to create
   queueLimit: 0
